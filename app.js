d2286c08d51ca80b09bf6febc4032778978a6f69
Jaime Salinas <jaime.salinas@supergirosatlantico.co>
2023-09-10 14:20:57 -0500

    subiendo cambios a la rama

--- app.js
+++ app.js
@@ -150,11 +150,3 @@ do {
 
 console.log('Hi ' + op + '!');
 
-
-
-
-
-/*var readlineSync = require('readline-sync'),
-  animals = ['Lion', 'Elephant', 'Crocodile', 'Giraffe', 'Hippo'],
-  index = readlineSync.keyInSelect(animals, 'Which animal?');
-console.log('Ok, ' + animals[index] + ' goes to your room.');*/
